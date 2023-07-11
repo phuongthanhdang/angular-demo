50f11fa52a57edb92377383aa596f929bcb8c207
phuongthanhdang <dangthiphuongthanh@gmail.com>
2023-07-11 17:36:29 +0700

    page home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../_services/user.service';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,15 @@ import { UserService } from '../_services/user.service';
 })
 export class HomeComponent implements OnInit {
   content?: string;
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private authService: AuthService
+  ) {}
+  name = '';
+  price: any;
+  message = false;
+  errorMessage = '';
+  products: any;
   ngOnInit(): void {
     this.userService.getPublicContent().subscribe(
       (data) => {
@@ -19,6 +28,15 @@ export class HomeComponent implements OnInit {
       }
     );
     console.log(this.content);
+    this.authService.searchProduct(this.name, this.price).subscribe(
+      (data) => {
+        // console.log(JSON.stringify(data));
+        this.products = data;
+      },
+      (err) => {
+        this.errorMessage = err.error.message;
+      }
+    );
   }
   reloadPage(): void {
     window.location.reload();
